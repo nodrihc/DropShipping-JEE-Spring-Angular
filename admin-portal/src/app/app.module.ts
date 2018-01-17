a1f7ebdbe5428f09bf042e1cbfc10ce6bcaf5afd
Chirdon <nodrihc93@gmail.com>
2018-01-17 14:19:51 +0000

    first part added

--- admin-portal/src/app/app.module.ts
+++ admin-portal/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 import { AppComponent } from './app.component';
+import {MatButtonModule, MatCheckboxModule} from "@angular/material";
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { MatToolbarModule} from "@angular/material";
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
